aaf6c0acc7bd66199c6708bcc92bb7857e56d39f
Elena Lavricheva <alexlav.elly@gmail.com>
2024-12-22 18:36:28 +0100

    looks ok I hope it is

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 02:02:10 by elavrich          #+#    #+#             */
-/*   Updated: 2024/12/21 21:10:58 by elavrich         ###   ########.fr       */
+/*   Updated: 2024/12/22 18:30:35 by elavrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,32 +27,42 @@ void	send_bits(int pid, char c)
 		else
 			kill(pid, SIGUSR2);
 		bit++;
-		usleep(250);
+		usleep(350);
 	}
+}
+
+void	partial(int pid, int argc, char *argv[])
+{
+	int	k;
+	int	i;
+
+	k = 2;
+	i = 0;
+	while (k < argc)
+	{
+		i = 0;
+		while (argv[k][i])
+		{
+			send_bits(pid, argv[k][i]);
+			i++;
+		}
+		send_bits(pid, ' ');
+		k++;
+	}
+	send_bits(pid, '\0');
 }
 
 int	main(int argc, char *argv[])
 {
 	int	pid;
-	int	i;
 	int	k;
 
 	k = 2;
-	i = 0;
 	pid = 0;
 	if (argc > 2)
 	{
 		pid = ft_atoi(argv[1]);
-		i = 0;
-		while (k < argc)
-		{
-			while (argv[k][i++])
-				send_bits(pid, argv[k][i]);
-			send_bits(pid, ' ');
-			i = 0;
-			k++;
-		}
-		send_bits(pid, '\0');
+		partial(pid, argc, argv);
 	}
 	else
 		return (ft_printf("Error\n"), 1);
